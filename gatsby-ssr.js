ce7d448ef1aeb43e4c0ff97093d50b29641756ac
User <user@pop-os.localdomain>
2022-12-24 10:34:10 +0700

    first commit

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,15 +1,19 @@
 const React = require('react')
 
-const HeadComponents = [
-  <script
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5738026098468973"
-    crossOrigin="anonymous"
-    async
-    key="google-adsense-script"
-  />
-]
+//const HeadComponents = [
+//  <script
+//    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5738026098468973"
+//    crossOrigin="anonymous"
+//    async
+//    key="google-adsense-script"
+//  />
+//]
 
-exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
+exports.onRenderBody = ({ setHtmlAttributes }) => {
   setHtmlAttributes({ lang: `id` })
-  setHeadComponents(HeadComponents)
 }
+
+//exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
+//  setHtmlAttributes({ lang: `id` })
+//  setHeadComponents(HeadComponents)
+//}

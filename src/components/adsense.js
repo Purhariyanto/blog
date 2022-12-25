1bcef7314fb5706558be5fbba66465a999b3bbc9
User <user@pop-os.localdomain>
2022-12-25 13:55:36 +0700

    first commit

--- src/components/adsense.js
+++ src/components/adsense.js
@@ -1,25 +1,19 @@
-import { useEffect, useRef } from 'react'
+import { useEffect } from "react"
 
 const Adsense = () => {
-    const scrolled = useRef(null)
 
-    useEffect(() => {
-        const headID = document.getElementsByTagName('head')[0]
+  useEffect(() => {
+    const headID = document.getElementsByTagName("head")[0]
 
-        document.addEventListener('scroll', function (e) {
-            if (!scrolled.current) {
-                scrolled.current = true
-                const gaScript = document.createElement('script')
-                gaScript.async = true
-                gaScript.crossorigin = 'anonymous'
-                gaScript['data-ad-client'] = 'ca-pub-5738026098468973'
-                gaScript.src =
-                    'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
-                gaScript.type = 'text/javascript'
-                headID.appendChild(gaScript)
-            }
-        })
-    }, [])
+    const gaScript = document.createElement("script")
+    gaScript.async = true
+    gaScript.crossorigin = "anonymous"
+    gaScript["data-ad-client"] = "ca-pub-5738026098468973"
+    gaScript.src =
+      "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    gaScript.type = "text/javascript"
+    headID.appendChild(gaScript)
+  }, [])
 }
 
 export default Adsense
